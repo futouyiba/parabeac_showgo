24213f6b95991a6c5d6f9a6cecd4e71189609894
Parabeac-Nest <admin@parabeac.com>
2022-11-25 09:39:09 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/parabeac_showgo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parabeac_showgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/parabeac_showgo_colors.g.dart
@@ -0,0 +1,50 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ParabeacShowgoColors {
+
+static const Color carbonNeutral300 = Color(0xffd6d9dd);
+
+static const Color carbonDarkest = Color(0xff24262b);
+
+static const Color systemOrangeLight = Color(0xffff9500);
+
+static const Color labelColorDarkPrimary = Color(0xffffffff);
+
+static const Color 100%白色 = Color(0xffffffff);
+
+static const Color 70%白色 = Color(0xffffffff);
+
+static const Color 品牌色 = Color(0xffffcc45);
+
+static const Color 背景色 = Color(0xff0b0a10);
+
+static const Color 50%白色 = Color(0xffffffff);
+
+static const Color 222 = Color(0xff222222);
+
+static const Color 按钮圆形底 = Color(0xff16141b);
+
+static const Color 90%白色文字 = Color(0xffffffff);
+
+static const Color 女生性别 = Color(0xffeb53c3);
+
+static const Color 5%白色 = Color(0xffffffff);
+
+static const Color 男生性别 = Color(0xff32c5ff);
+
+static const Color 10%白色 = Color(0xffffffff);
+
+static const Color 提醒 = Color(0xfff84364);
+
+static const Color 二级弹框底色 = Color(0xff1e1d28);
+
+static const Color 品牌色2 = Color(0xff0cdbc2);
+
+static const Color 30%白色 = Color(0xffffffff);
+}
